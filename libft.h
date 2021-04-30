13f075f83e4ee7afb657193808767e873f88bbfb
Maria Daan <mariadaan@gmail.com>
2021-04-30 11:39:21 +0200

    strstr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mdaan <mdaan@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/02 13:02:54 by mdaan         #+#    #+#                 */
-/*   Updated: 2021/03/23 15:12:42 by mdaan         ########   odam.nl         */
+/*   Updated: 2021/04/25 12:01:43 by mdaan         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,7 @@ int				ft_inset(char *s1, char c);
 int				ft_setinset(char *s1, char *s2);
 int				ft_modstr(char *s, char c);
 int				ft_strlend(const char *s, char end);
+char		*ft_strstr(const char *haystack, const char *needle);
 char			*ft_strnstr(const char *haystack, const char *needle,
 					size_t len);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
